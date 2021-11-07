39727cee6ab5409ad2fc1e06a2c853df7e9f1f59
Francisco Teixeira <franciscosft@gmail.com>
2021-11-07 09:30:11 -0300

    Pipeline test

--- src/test/java/com/dn/scanner/log/utils/ScannerUtilsTest.java
+++ src/test/java/com/dn/scanner/log/utils/ScannerUtilsTest.java
@@ -1,21 +1,41 @@
 package com.dn.scanner.log.utils;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.dn.scanner.log.App;
 import com.dn.scanner.log.util.ScannerUtils;
 
 public class ScannerUtilsTest {
 	
 	@Test
+	public void pocTest() {
+		File logFile = getLogFile();	
+		Assertions.assertNotNull(logFile);
+	}
+
+	private File getLogFile() {
+		try (InputStream resourceAsStream = App.class.getResourceAsStream("/server.log")) {
+    		File createTempFile = File.createTempFile("temp",".log");
+    		Files.copy(resourceAsStream, createTempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    		return createTempFile;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@Test
 	public void loadLogFileTest() throws URISyntaxException {
-		File loadLogFile = ScannerUtils.loadLogFile();
-		Assertions.assertNotNull(loadLogFile);
+//		File loadLogFile = ScannerUtils.loadLogFile();
+//		Assertions.assertNotNull(loadLogFile);
 		
 //		URL resource = ScannerUtils.class.getResource("/server.log");
 //		Path path = Path.of(resource.toURI());
