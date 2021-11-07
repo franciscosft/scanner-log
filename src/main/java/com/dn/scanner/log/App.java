37634a2e5afe4287c0ea50129fa50a389eaed1ad
Francisco Teixeira <franciscosft@gmail.com>
2021-11-07 09:14:01 -0300

    Solve problems to get a resource project

--- src/main/java/com/dn/scanner/log/App.java
+++ src/main/java/com/dn/scanner/log/App.java
@@ -1,19 +1,32 @@
 package com.dn.scanner.log;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 
+import com.dn.scanner.log.util.ScannerUtils;
+
 public class App {
     
     public static void main(String[] args) {
-    	URL resource = App.class.getResource("/server.log");
-    	try {
-			RenderingLogScanner.getRenderingStatistics(Path.of(resource.toURI()));
-		} catch (IOException | URISyntaxException e) {
-			System.err.println("It was not possible find server.log");
-		}
+    	long start = System.currentTimeMillis();
+    	
+    	File loadLogFile = ScannerUtils.loadLogFile();
+    	if (loadLogFile != null) {
+    		try {
+    			RenderingLogScanner.getRenderingStatistics(loadLogFile.toPath());
+    		} catch (IOException e) {
+    			System.err.println("It was not possible find server.log");
+    		}
+    	} else {
+    		System.err.println("It was not possible get server.log");
+    	}
+    	
+    	long end = System.currentTimeMillis();
+    	long executionTime = (end - start);
+		System.out.println("Execution time: " + executionTime + " milli seconds") ;
     }
-    
+
 }
---
+++ src/main/java/com/dn/scanner/log/util/ScannerUtils.java
@@ -0,0 +1,22 @@
+package com.dn.scanner.log.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class ScannerUtils {
+	
+	public static File loadLogFile() {
+		try (InputStream resourceAsStream = ScannerUtils.class.getResourceAsStream("/server.log")) {
+    		File createTempFile = File.createTempFile("temp",".log");
+    		Files.copy(resourceAsStream, createTempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    		return createTempFile;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
